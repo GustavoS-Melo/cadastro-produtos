657e31f23eace76f9367891c2b789fa41610d038
Gustavo <gustavo.souza.melo@hotmail.com>
2025-12-11 15:36:32 -0300

    correções e melhorias

--- script.js
+++ script.js
@@ -1,54 +1,58 @@
 class Produto {
-    constructor(){
+    constructor() {
         this.id = 1;
         this.arrayProdutos = [];
     }
 
-    Adicionar(){
-        let produto = this.lerDados()
-        if (this.Validar(produto)== true){
-            this.Salvar(produto)
+    Adicionar() {
+        let produto = this.lerDados();
+
+        if (this.Validar(produto)) {
+            this.Salvar(produto);
+            this.Listar();
+            this.Cancelar();
         }
-        this.Listar()
-        this.Cancelar()
     }
 
-    lerDados(){
-        let Produto = {}
+    lerDados() {
+        // Correção: criar o objeto com "produto" minúsculo
+        let produto = {};
 
         produto.id = this.id;
-        produto.nomeProduto = document.getElementById('pdnome').value
-        produto.precoProduto = document.getElementById('pdpreco').value
+        produto.nomeProduto = document.getElementById("pdnome").value;
+        produto.precoProduto = document.getElementById("pdpreco").value;
 
-        return produto
+        return produto;
     }
 
-    Validar(produto){
-        let msg = '';
+    Validar(produto) {
+        let msg = "";
 
-        if (produto.nomeProduto == '') {
-            msg += 'Insira o nome do produto \n'
+        if (produto.nomeProduto == "") {
+            msg += "Insira o nome do produto \n";
         }
-        if(produto.precoProduto == '') {
-            msg += 'Insira o preço o produto \n'
+        if (produto.precoProduto == "") {
+            msg += "Insira o preço do produto \n";
         }
-        if(msg != ''){
-            alert(msg)
-            return false
+
+        if (msg != "") {
+            alert(msg);
+            return false;
         }
-        return true
+
+        return true;
     }
 
-    Salvar(produto){
-        this.arrayProdutos.push(produto)
+    Salvar(produto) {
+        this.arrayProdutos.push(produto);
         this.id++;
     }
-    Listar(){
-        let tbody = document.getElementById('tbody')
-        tbody.innerText = ''
 
-        for(let i = 0; i < this.arrayProdutos.length; i++){
+    Listar() {
+        let tbody = document.getElementById("tbody");
+        tbody.innerText = "";
 
+        for (let i = 0; i < this.arrayProdutos.length; i++) {
             let tr = tbody.insertRow();
 
             let td_id = tr.insertCell();
@@ -59,27 +63,30 @@ class Produto {
             td_id.innerText = this.arrayProdutos[i].id;
             td_nome.innerText = this.arrayProdutos[i].nomeProduto;
             td_preco.innerText = this.arrayProdutos[i].precoProduto;
-            let imagem = document.createElement('img')
-            imagem.src = 'del.png'
-            imagem.setAttribute('onclick', 'produto.Deletar('+this.arrayProdutos[i].id+')')
-            td_del.appendChild(imagem)
+
+            let imagem = document.createElement("img");
+            imagem.src = "del.png";
+            imagem.classList.add("btn-del");
+            imagem.setAttribute("onclick", "produto.Deletar(" + this.arrayProdutos[i].id + ")");
+            
+            td_del.appendChild(imagem);
         }
     }
 
-    Cancelar(){
-        document.getElementById('pdnome').value = ''
-        document.getElementById('pdpreco').value = ''
+    Cancelar() {
+        document.getElementById("pdnome").value = "";
+        document.getElementById("pdpreco").value = "";
     }
 
-    Deletar(id){
-        let tbody = document.getElementById('tbody')
-        for (let i=0; i < this.arrayProdutos.length; i++){
-            if(this.arrayProdutos[i].id == id){
-                this.arrayProdutos.splice(i, 1)
-                tbody.deleteRow(i)
-            }
+    Deletar(id) {
+        // Encontra o índice do item
+        const index = this.arrayProdutos.findIndex(produto => produto.id === id);
+
+        if (index !== -1) {
+            this.arrayProdutos.splice(index, 1);
+            this.Listar(); // atualiza a tabela corretamente
+            alert("O item foi apagado com sucesso!");
         }
-        alert('O item foi apagado com sucesso')
     }
 }
 
